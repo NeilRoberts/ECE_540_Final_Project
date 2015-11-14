8701852a1231a434e437d75ae79bb046241fac85
NeilR <neilr@runbox.com>
2015-11-14 13:19:03 -0800

    Initialize project
    Create the empty project and add the top-level Verilog module.

---
+++ Rojobot_Live/Rojobot_Live.srcs/sources_1/new/rojobot_live.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/14/2015 01:16:20 PM
+// Design Name: 
+// Module Name: rojobot_live
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rojobot_live(
+
+    );
+endmodule
